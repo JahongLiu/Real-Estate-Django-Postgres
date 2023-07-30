e674e02b075d30d674c061756145e1ef8dfbfbf7
JahongLiu <jahongliu@gmail.com>
2023-07-30 15:48:41 -0400

    add listings urls

--- btre/urls.py
+++ btre/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 # If we want to define a path like this, we need to bring a path in
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
