d7ac77c54931421af2b61b778610c920027c2f44
JahongLiu <jahongliu@gmail.com>
2023-07-30 15:54:26 -0400

    fix bug (forget return)

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html')
     
 def listing(request):
-    render(request, 'listings/listing.html')
+    return render(request, 'listings/listing.html')
     
 def search(request):
-    render(request, 'listings/search.html')
+    return render(request, 'listings/search.html')
