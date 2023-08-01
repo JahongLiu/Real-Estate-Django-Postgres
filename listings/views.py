9f9d74d5c78d2b2e91f27c74916ada20e30883c5
JahongLiu <jahongliu@gmail.com>
2023-08-01 09:35:42 -0400

    fix the listings link

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 def index(request):
-    return render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html', {
+        'name': 'Brad'
+    })
     
 def listing(request):
     return render(request, 'listings/listing.html')
