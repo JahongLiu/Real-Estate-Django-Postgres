6eea145aeaa6c67d6433b61bdc175268fa68239a
JahongLiu <jahongliu@gmail.com>
2023-08-01 11:18:47 -0400

    pull data from listings model

--- listings/views.py
+++ listings/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html', {
-        'name': 'Brad'
-    })
+    listings = Listing.objects.all()
+    
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
     
 def listing(request):
-    return render(request, 'listings/listing.html')
+    return render(request, 'listin gs/listing.html')
     
 def search(request):
     return render(request, 'listings/search.html')
