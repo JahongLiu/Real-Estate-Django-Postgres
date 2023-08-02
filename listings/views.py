17fd3254c13bbaf87a397c1a7e2c1cf64c61194f
JahongLiu <jahongliu@gmail.com>
2023-08-02 15:47:16 -0400

    finish search form choices

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -25,4 +26,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
     
 def search(request):
-    return render(request, 'listings/search.html')
+    
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, 'listings/search.html', context)
