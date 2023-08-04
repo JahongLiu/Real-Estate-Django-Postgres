28854dc75e3ede12f4f46a07c170fc24b369ae96
JahongLiu <jahongliu@gmail.com>
2023-08-04 18:34:24 -0400

    delete local settings import,reset the local Settings File for deploy

--- btre/settings.py
+++ btre/settings.py
@@ -141,8 +141,3 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
