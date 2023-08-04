0bc5291fc3855b7c57c3d6b41559546b56fab165
JahongLiu <jahongliu@gmail.com>
2023-08-03 21:45:56 -0400

    refactor, set up urls, views and settings

--- btre/urls.py
+++ btre/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# If we want to define a path like this, we need to bring a path in
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
